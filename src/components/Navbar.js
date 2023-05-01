cc46915c4141cc760081483b59bc681b578af7d3
PlanetofJay <jae44dv@gmail.com>
2023-05-01 00:36:43 -0400

    change code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { FiMenu } from 'react-icons/fi';
-import './Navbar.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
 
-  const location = useLocation();
-
-  useEffect(() => {
-    setExpandNavbar(false);
-  }, [location]);
+  const handleExpandNavbar = () => setExpandNavbar(!expandNavbar);
+  const handleCloseNavbar = () => setExpandNavbar(false);
 
   return (
     <>
-      <nav className='navbar' id={expandNavbar ? 'open' : 'close'}>
+      <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo'>
-            GO travel
-            <img
-              src='../images/logo.png'
-              alt='logo'
-              width={30}
-              height={30}
-            />
+            GO travel <i className='fab fa-typo3' />
           </Link>
-          <div
-            className='menu-icon'
-            onClick={() => {
-              setExpandNavbar((prev) => !prev);
-            }}
-          >
-            <FiMenu />
+          <div className='menu-icon' onClick={handleExpandNavbar}>
+            <i className={expandNavbar ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={expandNavbar ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={handleCloseNavbar}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/services' className='nav-links' onClick={handleCloseNavbar}>
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/products' className='nav-links' onClick={handleCloseNavbar}>
+                Products
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/sign-up' className='nav-links-sign-up' onClick={handleCloseNavbar}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav >
     </>
