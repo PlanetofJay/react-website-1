be16498bf2c56b0228ce14601003409f3e0e468d
PlanetofJay <jae44dv@gmail.com>
2023-05-01 21:21:32 -0400

    working on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Button } from '../Button';
+import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -23,6 +25,18 @@ export default function Footer() {
           </form>
         </div>
       </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
