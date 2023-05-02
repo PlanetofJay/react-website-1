61c5cd1a4139c9152f064a5a2ea66ce2deada9ec
PlanetofJay <jae44dv@gmail.com>
2023-05-01 20:58:44 -0400

    update code, create footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Services from './pages/Services';
+import Products from './pages/Products';
+import SignUp from './pages/SignUp';
 
 export default function App() {
   return (
@@ -11,6 +14,9 @@ export default function App() {
         <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/sign-up' element={<SignUp />} />
           </Routes>
       </Router>
     </>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import '../../App.css';
 import HeroSection from '../../components/HeroSection';
 import Cards from '../../components/Cards';
+import Footer from '../../components/Footer';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer />
     </>
   );
 }
