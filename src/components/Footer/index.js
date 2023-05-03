8909f124878030108e7b35be40f6821d563387cf
PlanetofJay <jae44dv@gmail.com>
2023-05-02 22:33:47 -0400

    update footer code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Button } from '../Button';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import GithubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 export default function Footer() {
   return (
@@ -35,8 +40,58 @@ export default function Footer() {
             <Link to='/'>Investors</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
         </div>
       </div>
+      <section className='social-media'>
+        <div className='social-media-wrapper'>
+          <div className='footer-logo'>
+            <Link to='/' className='sicial-media-logo'>
+              GO travel
+            </Link>
+          </div>
+          <small className='website-rights'>&copy;2023 GO travel, all rights reserved.</small>
+          <div className='social-icons'>
+            <a href='https://github.com/PlanetofJay' target='_blank' rel="noreferrer">
+              <GithubIcon />
+            </a>
+            <a href='mailto:jae44dv@gmail.com'>
+              <EmailIcon />
+            </a>
+            <a href='https://linkedin.com/in/planetofjay/' target='_blank' rel="noreferrer">
+              <LinkedInIcon />
+            </a>
+            <a href='https://www.facebook.com/jae4f/' target='_blank' rel="noreferrer">
+              <FacebookIcon />
+            </a>
+            <a href='https://www.instagram.com/ofjaylee/' target='_blank' rel="noreferrer">
+              <InstagramIcon />
+            </a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
